e1a74b64c06b3ad4f95d444ffa196f7670fe61f9
Ap0c <Ap0c@users.noreply.github.com>
2016-03-23 16:23:19 +0000

    Added sqlite3 as a dep, introduced function and route for querying the db.

--- index.js
+++ index.js
@@ -2,19 +2,54 @@
 
 // ----- Requires ----- //
 
-var express = require('express');
+let express = require('express');
+let sqlite3 = require('sqlite3').verbose();
 
 
 // ----- Setup ----- //
 
-var app = express();
+// Sqlite database.
+let DB_FILE = 'music.db';
+
+let app = express();
 app.use('/static', express.static('static'));
+
+
+// ----- Functions ----- //
+
+// General-purpose function for querying the database.
+function dbQuery (sql, params) {
+
+	return new Promise((res, rej) => {
+
+		let db = new sqlite3.Database(DB_FILE);
+		let result = null;
+
+		db.serialize(runQuery);
+
+		function runQuery () {
+			db.all(sql, params, handle);
+		}
+
+		function handle (err, rows) {
+			res(rows);
+		}
+
+		db.close();
+
+	});
+
+}
 
 
 // ----- Routes ----- //
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/app.html');
+});
+
+app.get('/db', (req, res) => {
+	res.send('Database result');
 });
 
 
