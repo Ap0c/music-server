ecc85f717dd0bd75e267963883bd389c9e908db5
Ap0c <Ap0c@users.noreply.github.com>
2016-03-23 15:25:21 +0000

    Added schema file for database.

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS artists
+	(id INTEGER PRIMARY KEY, name TEXT);
+
+CREATE TABLE IF NOT EXISTS albums
+	(id INTEGER, name TEXT, artist INTEGER,
+		FOREIGN KEY (artist) REFERENCES artists(id));
+
+CREATE TABLE IF NOT EXISTS songs
+	(id INTEGER PRIMARY KEY, name TEXT, artist INTEGER, album INTEGER, url TEXT,
+		FOREIGN KEY (artist) REFERENCES artists(id),
+		FOREIGN KEY (album) REFERENCES albums(id));
