a6ae06ca38959085f74740f949b1e266bcaf67e4
Ap0c <Ap0c@users.noreply.github.com>
2016-03-23 15:35:23 +0000

    Filled out index with dummy express app.

--- index.js
+++ index.js
@@ -3,3 +3,22 @@
 // ----- Requires ----- //
 
 var express = require('express');
+
+
+// ----- Setup ----- //
+
+var app = express();
+
+
+// ----- Routes ----- //
+
+app.get('/', (req, res) => {
+	res.send('hello world');
+});
+
+
+// ----- Run ----- //
+
+app.listen(3000, () => {
+	console.log('Running on 3000...');
+});
