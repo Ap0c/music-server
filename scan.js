72d0d64554bc7608447f470588c32153641802e5
Ap0c <Ap0c@users.noreply.github.com>
2016-03-24 17:58:17 +0000

    Added function to read artists and helper function to check if path is dir asynchronously.

--- scan.js
+++ scan.js
@@ -3,15 +3,67 @@
 // ----- Requires ----- //
 
 let fs = require('fs');
+let path = require('path');
 
 let Db = require('./db');
 
 
 // ----- Functions ----- //
 
+// Checks if something is a directory.
+function isDir (path, directory) {
+
+	return new Promise((res, rej) => {
+
+		let fullpath = path.join(path, directory);
+
+		fs.stat(fullpath, (err, stats) => {
+
+			if (err) {
+				rej(err);
+			} else {
+				res(stats.isDirectory());
+			}
+
+		});
+
+	});
+
+}
+
+function readArtists (library) {
+
+	let artists = [];
+	let libraryPath = library.path;
+
+	fs.readdir(library, (err, files) => {
+
+		let checkDirs = [];
+
+		for (let file of files) {
+			checkDirs.push(isDir(library, file));
+		}
+
+		Promise.all(checkDirs).then((valids) => {
+
+			for (var i = files.length - 1; i >= 0; i--) {
+
+				if (valids[i]) {
+					artists.append({ name: files[i], dirname: files[i] });
+				}
+
+			}
+
+		});
+
+	});
+
+}
+
 function diffMusic (db, library, currentSongs) {
 
-
+	let toAdd = [];
+	let artists = readArtists(library);
 
 }
 
@@ -22,7 +74,7 @@ function syncMusic (db, library) {
 
 		diffMusic(db, library, stored_songs).then((songsDiff) => {
 
-			let insertions = [
+			let synchronisations = [
 				db.many(`INSERT INTO songs (name, artist, album, path)
 					VALUES ($name, $artist, $album, $path)`, songsDiff.add),
 				db.many('DELETE FROM songs WHERE id = ?', songsDiff.delete),
@@ -30,7 +82,7 @@ function syncMusic (db, library) {
 				db.many('DELETE FROM albums WHERE id = ?', songsDiff.albums)
 			];
 
-			Promise.all(insertions);
+			Promise.all(synchronisations);
 
 		});
 
