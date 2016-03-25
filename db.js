94b4cefd40301e06ee9fcc5a2943d191dbd7d681
Ap0c <Ap0c@users.noreply.github.com>
2016-03-25 18:31:37 +0000

    Restructured database module to use a wrapper function for promisification.

--- db.js
+++ db.js
@@ -14,123 +14,113 @@ module.exports = function Db (dbFile) {
 
 	let db = null;
 
+	// ----- Promise Wrapper ----- //
+
+	// Wrapper for promisifications and checking db connection is open.
+	function wrap (fn) {
+
+		return function wrapper () {
+
+			return new Promise((res, rej) => {
+
+				if (!db) {
+					rej('Database not open.');
+				}
+
+				fn(res, rej, ...arguments);
+
+			});
+
+		};
+
+	}
 
 	// ----- Functions ----- //
 
 	// Connects to the database.
-	function connectDb () {
+	let connectDb = () => {
 		db = new sqlite3.Database(dbFile);
-	}
+	};
 
 	// Closes the database connection.
-	function closeDb () {
+	let closeDb = () => {
 
 		db.close();
 		db = null;
 
-	}
+	};
 
 	// Creates the database from the schema.
-	function initDb (dbSchema) {
+	let initDb = wrap((res, rej, dbSchema) => {
 
-		return new Promise((res, rej) => {
+		fs.readFile(dbSchema, 'utf8', (err, data) => {
 
-			if (!db) {
-				rej('Database not open.');
+			if (err) {
+				rej(err);
 			}
 
-			fs.readFile(dbSchema, 'utf8', (err, data) => {
+			db.exec(data, (err) => {
 
 				if (err) {
 					rej(err);
+				} else {
+					res();
 				}
-
-				db.exec(data, (err) => {
-
-					if (err) {
-						rej(err);
-					} else {
-						res();
-					}
-
-				});
 
 			});
 
 		});
 
-	}
+	});
 
 	// General-purpose function for querying the database.
-	function dbQuery (sql, params) {
+	let dbQuery = wrap((res, rej, sql, params) => {
 
-		return new Promise((res, rej) => {
+		db.all(sql, params, handle);
 
-			if (!db) {
-				rej('Database not open.');
+		function handle (err, rows) {
+
+			if (err) {
+				rej(err);
+			} else {
+				res(rows);
 			}
 
-			db.all(sql, params, result);
+		}
 
-			function result (err, rows) {
-
-				if (err) {
-					rej(err);
-				} else {
-					res(rows);
-				}
-
-			}
-
-		});
-
-	}
+	});
 
 	// Runs an insert query and resolves with the row id.
-	function dbInsert (sql, params) {
+	let dbInsert = wrap((res, rej, sql, params) => {
 
-		return new Promise((res, rej) => {
+		db.run(sql, params, handle);
 
-			if (!db) {
-				rej('Database not open.');
+		function handle (err) {
+
+			if (err) {
+				rej(err);
+			} else {
+				res(this.lastID);
 			}
 
-			db.run(sql, params, handle);
+		}
 
-			function handle (err) {
-
-				if (err) {
-					rej(err);
-				} else {
-					res(this.lastID);
-				}
-
-			}
-
-		});
-
-	}
+	});
 
 	// Runs multiple queries.
-	function dbMany (sql, params) {
+	let dbMany = wrap((res, rej, sql, params) => {
 
-		return new Promise((res, rej) => {
+		let counter = 0;
 
-			if (!db) {
-				rej('Database not open.');
-			}
+		for (let paramset of params) {
+			db.run(sql, paramset, handle);
+		}
 
-			let counter = 0;
+		function handle (err) {
 
-			for (let paramset of params) {
-				db.run(sql, paramset, handle);
-			}
-
-			function handle (err) {
-
-				if (err) {
-					rej(err);
-				}
+			if (err) {
+				rej(err);
+			} else {
 
 				counter++;
 
@@ -140,9 +130,9 @@ module.exports = function Db (dbFile) {
 
 			}
 
-		});
+		}
 
-	}
+	});
 
 	// ----- Interface ----- //
 
