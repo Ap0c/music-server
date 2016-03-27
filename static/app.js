e080136b18b584015dd433fdcd83d81233e9271a
Ap0c <Ap0c@users.noreply.github.com>
2016-03-27 21:34:05 +0100

    Removed frontend route handling, as it's now managed by a tags.

--- static/app.js
+++ static/app.js
@@ -16,44 +16,20 @@ var views = (function Views () {
 
 // ----- Functions ----- //
 
-// Retrieves the urls for list view items.
-function navRoutes (currentView, id) {
-
-	if (currentView === 'libraries') {
-		location.assign(`/library/${id}`);
-	} else if (currentView === 'artists') {
-		location.assign(`/artist/${id}`);
-	} else if (currentView === 'artist' || currentView === 'albums') {
-		location.assign(`/album/${id}`);
-	} else if (currentView === 'album') {
-		console.log('Play song!');
-	} else if (currentView === 'songs') {
-		console.log('Play song!');
-	}
-
-}
-
 // Sets up navigation via click in the nav section.
 function navClicks () {
 
 	var nav = document.getElementById('navigation');
-	var view = nav.dataset.view;
 
 	nav.addEventListener('click', function (event) {
 
 		var target = event.target;
 		var id = target.parentNode.dataset.id;
 
-		if (target.tagName === 'SPAN') {
-
-			if (target.className === 'list-name') {
-
-				if (!clientRouting) {
-					navRoutes(view, id);
-				}
-
-			}
-
+		if (target.classList.contains('song')) {
+			console.log(`Play song ${id}`);
+		} else if (target.className === 'plus') {
+			console.log(`Queue item ${id}`);
 		}
 
 	});
